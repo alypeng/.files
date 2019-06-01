1e0cbc15eb7bff88264332e70d130a03fcf39c51
alypeng <6505987+alypeng@users.noreply.github.com>
2019-06-01 13:14:16 +0100

    Add .config/fish/config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+switch (uname)
+    case Darwin
+        set -x PATH /usr/local/sbin $PATH
+
+        set -x PATH /usr/local/opt/php@7.2/bin $PATH
+        set -x PATH /usr/local/opt/php@7.2/sbin $PATH
+
+        set -x PATH /usr/local/opt/python/libexec/bin $PATH
+
+    case Linux
+        set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
+        set -x PATH /home/linuxbrew/.linuxbrew/sbin $PATH
+
+        set -x PATH /home/linuxbrew/.linuxbrew/opt/php@7.2/bin $PATH
+        set -x PATH /home/linuxbrew/.linuxbrew/opt/php@7.2/sbin $PATH
+
+        set -x PATH /home/linuxbrew/.linuxbrew/opt/python/libexec/bin $PATH
+end
