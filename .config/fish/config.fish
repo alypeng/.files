b9e52e0649b6c264144ae0cba10d4ab2487cc845
alypeng <6505987+alypeng@users.noreply.github.com>
2019-08-25 18:04:35 +0100

    Fix PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,20 @@
 switch (uname)
     case Darwin
-        set -x PATH /usr/local/sbin $PATH
+        set -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
 
-        set -x PATH /usr/local/opt/php@7.2/bin $PATH
         set -x PATH /usr/local/opt/php@7.2/sbin $PATH
+        set -x PATH /usr/local/opt/php@7.2/bin $PATH
 
         set -x PATH /usr/local/opt/python/libexec/bin $PATH
 
     case Linux
-        set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
-        set -x PATH /home/linuxbrew/.linuxbrew/sbin $PATH
+        set -x PATH /usr/local/bin /usr/bin /usr/local/sbin /usr/sbin
 
-        set -x PATH /home/linuxbrew/.linuxbrew/opt/php@7.2/bin $PATH
+        set -x PATH /home/linuxbrew/.linuxbrew/sbin $PATH
+        set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
+
         set -x PATH /home/linuxbrew/.linuxbrew/opt/php@7.2/sbin $PATH
+        set -x PATH /home/linuxbrew/.linuxbrew/opt/php@7.2/bin $PATH
 
         set -x PATH /home/linuxbrew/.linuxbrew/opt/python/libexec/bin $PATH
 end
