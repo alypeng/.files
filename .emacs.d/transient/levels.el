a204836af1cbdb0059b482897ac9f9399f4bdc26
alypeng <6505987+alypeng@users.noreply.github.com>
2020-03-12 12:50:07 +0000

    Add transient levels and values

---
+++ .emacs.d/transient/levels.el
@@ -0,0 +1,4 @@
+((magit-diff
+  (magit-diff:--color-moved . 4))
+ (magit-diff-refresh
+  (magit-diff:--color-moved . 4)))
---
+++ .emacs.d/transient/values.el
@@ -0,0 +1,3 @@
+((magit-diff:magit-diff-mode "--no-ext-diff" "--stat" "--color-moved=default")
+ (magit-diff:magit-status-mode "--no-ext-diff" "--color-moved=default")
+ (magit-fetch "--prune"))
