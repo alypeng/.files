cc2a3a72515fbe8ca6d25cae97e0e01b8c28c710
alypeng <6505987+alypeng@users.noreply.github.com>
2020-06-06 19:10:26 +0100

    Remove emoji from fish_prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,8 +1,13 @@
 function fish_prompt
+    set -l last_status $status
     set -l branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
 
     echo
-    echo -ns '🐧'
+    if test $last_status -ne 0
+        echo -ns ':o'
+    else
+        echo -ns ':)'
+    end
     echo -ns ' '
     echo -ns (set_color $fish_color_cwd) (prompt_pwd)
     echo -ns ' '
