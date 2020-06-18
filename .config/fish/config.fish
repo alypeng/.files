6a8103b06295e80a0a0be4e1d7f6201f4f6dfb31
alypeng <6505987+alypeng@users.noreply.github.com>
2020-06-18 15:23:50 +0100

    Set environment variables for login shells only

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,31 +1,31 @@
-switch (uname)
-    case Darwin
-        set -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
-
-        set -x ASPELL_CONF "data-dir $HOME/.nix-profile/lib/aspell"
-
-    case Linux
-        set -x PATH /usr/local/bin /usr/bin /usr/local/sbin /usr/sbin
-
-        set -x TERM xterm-256color
-end
-
-set fish_function_path $fish_function_path \
-    "$HOME"/.nix-profile/share/fish-foreign-env/functions
-
-fenv source "$HOME"/.nix-profile/etc/profile.d/nix.sh
-
-set -x PATH "$HOME"/bin "$HOME"/.npm/bin $PATH
-
-set -x LESSHISTFILE /dev/null
-set -x PREFIX "$HOME"/.npm
-
-direnv hook fish | source
-
-solarized_light
-
 if status is-login
+    switch (uname)
+        case Darwin
+            set -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
+
+            set -x ASPELL_CONF "data-dir $HOME/.nix-profile/lib/aspell"
+
+        case Linux
+            set -x PATH /usr/local/bin /usr/bin /usr/local/sbin /usr/sbin
+
+            set -x TERM xterm-256color
+    end
+
+    set fish_function_path $fish_function_path \
+        "$HOME"/.nix-profile/share/fish-foreign-env/functions
+
+    fenv source "$HOME"/.nix-profile/etc/profile.d/nix.sh
+
+    set -x PATH "$HOME"/bin "$HOME"/.npm/bin $PATH
+
+    set -x LESSHISTFILE /dev/null
+    set -x PREFIX "$HOME"/.npm
+
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
         exec startx
     end
 end
+
+direnv hook fish | source
+
+solarized_light
