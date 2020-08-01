7d08d460c604ae5a871bf67d4034e21f60c2953b
alypeng <6505987+alypeng@users.noreply.github.com>
2020-08-01 11:13:23 +0100

    Simplify fish_prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,19 +1,7 @@
 function fish_prompt
-    set -l last_status $status
-    set -l branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
-
     echo
-    if test $last_status -ne 0
-        echo -ns ':o'
-    else
-        echo -ns ':)'
-    end
-    echo -ns ' '
     echo -ns (set_color $fish_color_cwd) (prompt_pwd)
     echo -ns ' '
-    echo -ns (set_color $fish_color_quote) $branch
-
-    echo
     echo -ns (set_color magenta) '->'
     echo -ns ' '
     echo -ns (set_color normal)
